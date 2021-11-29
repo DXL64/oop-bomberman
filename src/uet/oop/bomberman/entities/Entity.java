0335f3a10d8ff7ecb530e3063c3bce77e4f4377f
Loc Dang <locybdxlyb204@gmail.com>
2021-11-29 23:36:14 +0700

    CollisionManager

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -1,10 +1,8 @@
 package uet.oop.bomberman.entities;
 
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.controller.Camera;
 import uet.oop.bomberman.graphics.Sprite;
 
 public abstract class Entity {
@@ -45,6 +43,11 @@ public abstract class Entity {
     public void render(GraphicsContext gc) {
         gc.drawImage(img, x, y);
     }
+
+    // public void render(GraphicsContext gc, Camera camera) {
+    //     gc.drawImage(img, x - camera.getX(), y - camera.getY());
+    // }
+    public abstract void render(GraphicsContext gc, Camera camera);
     
     public abstract void update();
 
