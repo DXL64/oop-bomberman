fb8c5ce4db04cd15491f8fd9812c4816ba1c526d
phamgialinhlx <phamgialinhlx2@gmail.com>
2021-12-09 21:35:16 +0700

    Fix sprite of portal

--- src/uet/oop/bomberman/entities/Portal.java
+++ src/uet/oop/bomberman/entities/Portal.java
@@ -2,7 +2,10 @@ package uet.oop.bomberman.entities;
 
 import javax.xml.stream.events.StartDocument;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import uet.oop.bomberman.controller.Camera;
+import uet.oop.bomberman.graphics.Sprite;
 
 public class Portal extends Tile {
     public static final int code = 4;
@@ -15,5 +18,11 @@ public class Portal extends Tile {
         // TODO Auto-generated method stub
         
     }
+
+    @Override
+    public void render(GraphicsContext gc, Camera camera) {
+        gc.drawImage(Sprite.grass.getFxImage(), x - camera.getX(), y - camera.getY());
+        super.render(gc, camera);
+    }    
 
 }
